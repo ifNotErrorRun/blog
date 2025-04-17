0279a5056033a4021f39bfece98ec1a3b42d3c67
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-04-18 00:15:07 +0900

    add: tiptap editor

--- src/pages/(auth)/articles.new.route.tsx
+++ src/pages/(auth)/articles.new.route.tsx
@@ -1,18 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router';
-import Tiptap from '@/features/editor/Tiptap';
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 
 export const Route = createFileRoute('/(auth)/articles/new')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const editor = useEditor({
+		extensions: [StarterKit],
+		content: '<p>Hello World!</p>',
+	});
+
 	return (
 		<>
 			<div className='flex flex-col items-center justify-center gap-y-3 mx-6'>
 				<h3 className='text-2xl font-bold'>New Article</h3>
 				<p className='text-sm text-gray-500'>Create a new article</p>
 				<div className='card'>
-					<Tiptap />
+					<EditorContent editor={editor} />
 				</div>
 			</div>
 		</>
